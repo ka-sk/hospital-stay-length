4d7f649ec7b752294515464b1ee68535922669ce
ka-sk <katarz.szczepaniak@gmail.com>
2025-10-09 15:46:46 +0200

    Preparation of file and folder structure

---
+++ main.py
@@ -0,0 +1,11 @@
+# Running whole experiment
+
+# data aquisition
+
+# data cleaning and preprocessing
+
+# loading models and their hyperparameters
+
+# looping over every model
+
+# looping over hyperparameters
