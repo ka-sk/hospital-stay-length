33c0a843385d45f48393c32f13d6b72b19d3cfa9
ka-sk <katarz.szczepaniak@gmail.com>
2025-10-11 12:27:57 +0200

    loading and filtering of data in data_loader.py file

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,35 @@
-#Data aquisition
 import kagglehub
+import pandas as pd
+from pathlib import Path
 
-# Download latest version
-path = kagglehub.dataset_download("abdallaahmed77/healthcare-risk-factors-dataset")
 
-print("Path to dataset files:", path)
+def load_data():
+    
+    # Download data
+    path = Path(kagglehub.dataset_download("abdallaahmed77/healthcare-risk-factors-dataset"))
+    
+    # Load data
+    dataframe = pd.read_csv(path / 'dirty_v3_path.csv')
+    
+    return dataframe
+
+
+def data_filtration(dataframe: pd.DataFrame):
+    # filter out unnecessary colums 
+    dataframe = dataframe.drop(['random_notes', 'noise_col'], axis=1)
+
+    # nb of rows before filtration
+    len1 = dataframe.shape[0]
+
+    # filtrations of lacking data
+    dataframe = dataframe.dropna()
+    # nb of rows after filtration
+    len2 = dataframe.shape[0]
+
+    print(f"Rows with lacking data: {len1-len2}")
+
+    return dataframe
+
+
+if __name__ == "__main__":
+    data_filtration(load_data())
